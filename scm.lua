5a99ac9e70b871ac5cd5269125a314c9997b4a2b
Josh <dev@cldv.org>
2022-07-08 00:46:10 +0200

    prepare download variants

--- scm.lua
+++ scm.lua
@@ -23,6 +23,7 @@ scm.commands = {
     ["add"] = {
         func = function ()
             --@TODO: Function to add a library via Git, Pastebin or URL
+            self:download(args[2], "library")
         end,
         desc = [[
 Adds a library with all its dependencies.
@@ -39,6 +40,64 @@ add <URL>
     ["config"] = {},
     ["help"] = {}
 }
+
+
+
+function scm:download (target, fileType)
+    if target == nil then 
+        --@TODO: Error handling
+        return false
+    end
+    
+    -- Check for Pastebin
+    local separator = string.find(target, "@")
+    if separator then
+        local name = string.sub(target, 1, separator - 1)
+        local code = string.sub(target, separator + 1)
+        return self:downloadPastebin(name, code)
+    end
+
+    -- Check for URL
+    local isURL = string.lower(string.sub(target, 0, 4)) == "http"
+    if isURL then
+        return self:downloadURL(target)
+    end
+
+    -- We assume it's Git
+    -- The suffix is used to find the correct repository on GitHub
+    local suffix
+    if fileType == "library" then
+        suffix = self.config["librarySuffix"]
+    else
+        suffix = self.config["programSuffix"]
+    end
+    local repository = target .. suffix
+
+    return self:downloadGit(target, repository)
+end
+
+function scm:downloadGit (name, repository)
+    --@TODO: Download a library or program from Git
+    local URL = self.config["rawURL"] .. 
+                self.config["user"] .. "/" .. 
+                repository .. "/" .. 
+                self.config["branch"] .. "/" .. 
+                name .. ".lua"
+    --@TODO: Return false on error, true on success
+    return false
+end
+
+function scm:downloadPastebin (name, code)
+    --@TODO: Download file from Pastebin
+
+    --@TODO: Return false on error, true on success
+    return false
+end
+
+function scm:downloadURL (url)
+    --@TODO: Return false on error, true on success
+    return false
+end
 
 
 return scm
