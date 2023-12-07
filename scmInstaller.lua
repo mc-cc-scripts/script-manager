04ec0d6fab509b170565ee1b8da5d91bec066f01
ChristophLHR <11349102+ChristophLHR@users.noreply.github.com>
2023-12-07 19:40:34 +0100

    Updated Installer

--- scmInstaller.lua
+++ scmInstaller.lua
@@ -2,23 +2,23 @@
 ---@class SCMInstaller
 local SCMInstaller = {}
 
-
-local files = {}
-local source = "raw.githubusercontent.com/mc-cc-scripts/script-manager/Issue-30-Spilt-SCM/"
-
-
-function SCMInstaller:getFilesTxt()
-    http.get(source .. "files.txt", nil, function(response)
-        local file = response.readLine()
-        while file ~= nil do
-            table.insert(files, file)
-            file = response.readLine()
-        end
-        response.close()
-    end)
+function SCMInstaller:getFilesTxt(source)
+    local files = {}
+    print("Downloading from " .. source .. "files.txt")
+    local response = http.get(source .. "files.txt")
+    if response == nil or response.getResponseCode() ~= 200 then
+        error("Failed to download files.txt")
+    end
+    local file = response.readLine()
+    while file ~= nil do
+        table.insert(files, file)
+        file = response.readLine()
+    end
+    response.close()
+    return files
 end
 
-function SCMInstaller:delteFiles()
+function SCMInstaller:deleteFiles(files)
     for _, value in ipairs(files) do
         print("Deleting File " .. value)
         if fs.exists(value) then
@@ -28,15 +28,17 @@ function SCMInstaller:delteFiles()
 end
 
 -- download the files
-function SCMInstaller:downloadFiles()
+function SCMInstaller:downloadFiles(source, files)
     for index, value in ipairs(files) do
-        http.get(source .. value, nil, function(response)
-            print('Downloading ' .. index .. ' of ' .. #files .. ' files: ' .. value)
-            local file = fs.open(value, "w")
-            file.write(response.readAll())
-            file.close()
-            response.close()
-        end)
+        print('Downloading ' .. index .. ' of ' .. #files .. ' files: ' .. value)
+        local response = http.get(source .. value)
+        if not response or response.getResponseCode() ~= 200 then
+            error("Failed to download " .. value)
+        end
+        local file = fs.open(value, "w")
+        file.write(response.readAll())
+        file.close()
+        response.close()
     end
 end
 
